35a5766d37653a40643ea6183050a9acfbec6260
mrfatbat <88135284+mrfatbat@users.noreply.github.com>
2021-08-15 18:00:09 +1000

    update mrcoolpi_temp.py

--- mrcoolpi_temp.py
+++ mrcoolpi_temp.py
@@ -5,10 +5,25 @@ from gpiozero import CPUTemperature
 #GPIO for Status LEDS
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(11,GPIO.OUT) #HEAT LED
+
 	
 def raspi_cpu_temp():
 	print("Obtaining RasPi CPU Temp....")
 	cpu = CPUTemperature()
 	print(cpu.temperature)
+	
+def __init__(self,tmp_snsr_name,tmp_snsr_type,tmp_snsr_io):
+  self.name = tmp_snsr_name
+  self.type = tmp_snsr_type
+  self.io = tmp_snsr_io
+  self.tmp = 0
+  self.last_tmp = 0
+  self avg_tmp = 0
+  if self.type = "RPiCPU":
+    return pass
+  
+  
+  
+  
+
 
